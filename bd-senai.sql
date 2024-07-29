e44ec148f17c95bb32c465f4c6c66edcf3b90984
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2024-07-29 16:09:07 -0300

    Add bd-senai

---
+++ bd-senai.sql
@@ -0,0 +1,31 @@
+-- create database bd_senai;
+
+use bd_senai;
+show databases;
+
+create table aluno (
+	id_aluno int not null primary key auto_increment,
+    nome varchar(60) not null,
+    sobrenome varchar(60) not null
+)default charset utf8;
+
+create table professor (
+	id_professor int not null primary key auto_increment,
+    nome varchar(60) not null, 
+    sobrenome varchar(60) not null
+)default charset utf8;
+
+create table turma(
+	id_turma int not null auto_increment primary key,
+    id_aluno int,
+    foreign key (id_aluno) references aluno(id_aluno),
+    id_professor int,
+    foreign key (id_professor) references professor(id_professor)
+) default charset utf8mb4;
+
+show tables;
+
+select * from turma;
+
+-- drop table professor;
+-- drop table aluno;
